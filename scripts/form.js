30e500d6feb514d6551427af555a5abd134bcb18
Matthew Meade <mafumeade@gmail.com>
2017-04-23 16:38:18 -0230

    Finish Contact Form

---
+++ scripts/form.js
@@ -0,0 +1,60 @@
+var headings, button;
+var nameInput, subjectInput;
+var form;
+
+(function(){
+  document.querySelector("form").addEventListener("submit", OnFormSubmit);
+  headings = document.querySelectorAll("#contact h2");
+  button = document.querySelector("#submitButton");
+
+  nameInput = document.querySelector("#name");
+  subjectInput = document.querySelector("#subject");
+
+  form = document.querySelector("#contactForm");
+})();
+
+
+function OnFormSubmit(event){
+  event.preventDefault();
+
+  toggleHeader();
+
+  var http = new XMLHttpRequest();
+  var url = "https://formspree.io/mafumeade@gmail.com";
+
+  subjectInput.value += nameInput.value;
+
+  var formData = new FormData(form);
+
+  http.open("POST", url, true);
+
+  http.setRequestHeader('accept', 'application/json');
+
+  http.onreadystatechange = function() {
+      if(http.readyState === 4) {
+          setTimeout(toggleHeader, 1000);
+          form.reset();
+      }
+  };
+
+  button.value = "Sending...";
+  http.send(formData);
+
+}
+
+function toggleHeader(){
+  if(headings[0].className === ""){
+    headings[0].className = "hidden";
+    headings[1].className = "";
+
+    button.value = "Thank you";
+  }else{
+    headings[0].className = "";
+    headings[1].className = "hidden";
+    button.value = "Submit";
+  }
+
+  button.className = headings[0].className;
+
+
+}
--- scripts/scroll.js
+++ scripts/scroll.js
@@ -10,19 +10,11 @@ function update(){
 
   var pos = window.pageYOffset;
 
-
-  if(pos > 20){
-    header.className = "mini";
-    spacer.className = "enabled";
-  }else{
-    header.className = "";
-    spacer.className = "";
-  }
-
+  header.className = pos > 20 ? "mini" : "";
+  spacer.className = pos > 20 ? "enabled" : "";
 
   images.forEach(function(elem, i){
     var top = elem.parentElement.getBoundingClientRect().top;
-    var bottom = elem.parentElement.getBoundingClientRect().bottom;
 
     if(top < pOff){
       elem.style.top = -1 * ((pos + pOff) - (top + pos)) + 'px';
