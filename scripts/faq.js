f9bf49b91269fbb46d293deadd031f22b36d89ea
Matthew Meade <mafumeade@gmail.com>
2017-04-24 22:02:37 -0230

    Add FAQ Styles and Script

---
+++ scripts/faq.js
@@ -0,0 +1,34 @@
+var questions = document.querySelector("#faq ul");
+var background = document.querySelector("#faqBg");
+var faqWindow = document.querySelector("#faq");
+
+(function(){
+    questions.addEventListener("click", function(e){
+       if(e.srcElement.tagName === "H3"){
+
+           var s = document.querySelector("li.active");
+           if(s) s.className = "";
+
+           var parent = e.srcElement.parentNode;
+           parent.className = parent.className === "active" ? "" : "active";
+           
+       }
+    });
+})();
+
+(function () {
+    background.addEventListener("click", function(){
+       toggleWindow(false);
+    });
+
+    document.querySelector("#faqClose").addEventListener("click", function () {
+        toggleWindow(false);
+    })
+})();
+
+
+function toggleWindow(mode){
+    document.body.style.overflow = mode ? "hidden" : "";
+    background.className = mode ? "enabled" : "";
+    faqWindow.className = mode ? "enabled" : "";
+}
