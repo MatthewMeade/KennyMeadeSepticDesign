61dfb420b92b9823c7c6434e0fd68c4272348001
Matthew Meade <mafumeade@gmail.com>
2017-05-21 23:16:38 -0230

    Improve Header animation

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,6 +1,6 @@
 var images = document.querySelectorAll(".pSection .bg");
-var header = document.querySelector("header");
-var spacer = document.querySelector("#headSpacer");
+var largeHeader = document.querySelector("#largeHeader");
+var smallHeader = document.querySelector("#smallHeader");
 var background = document.querySelector("#pageBackground");
 var pOff = 400;
 
@@ -11,8 +11,17 @@ function update(){
 
   var pos = window.pageYOffset;
 
-  header.className = pos > 20 ? "mini" : "";
-  spacer.className = pos > 20 ? "enabled" : "";
+
+  if(pos > 90){
+    largeHeader.className = "disabled";
+    smallHeader.className = "enabled";
+  }
+
+  if(pos < 20){
+    largeHeader.className = "enabled";
+    smallHeader.className = "disabled";
+  }
+
 
   images.forEach(function(elem, i){
     var top = elem.parentElement.getBoundingClientRect().top;
