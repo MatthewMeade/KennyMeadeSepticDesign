b22eb044c70fca3eecbba2d0aca37eb412d0bec7
Matthew Meade <mafumeade@gmail.com>
2017-05-26 23:22:28 -0230

    Add icons to home page

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -2,7 +2,8 @@ var images = document.querySelectorAll(".pSection .bg");
 var largeHeader = document.querySelector("#largeHeader");
 var smallHeader = document.querySelector("#smallHeader");
 // var background = document.querySelector("#pageBackground");
-var pOff = 400;
+var icons = document.querySelectorAll(".pSection+i");
+var pOff = 200;
 
 window.addEventListener("scroll", update);
 
@@ -30,6 +31,13 @@ function update(){
       elem.style.top = -1 * ((pos + pOff) - (top + pos)) + 'px';
     }
   });
+
+  var top0 = icons[0].getBoundingClientRect().top;
+  icons[0].style.textShadow = `15px ${Math.min(top0 / 6, 30)}px 10px #BBB`;
+
+
+  var top1 = icons[1].getBoundingClientRect().top - 200;
+  icons[1].style.textShadow = `15px ${Math.min(top1 / 6, 30)}px 10px #BBB`;
 
   // background.style.top = (pos / 1.5) - 300 + "px";
 }
